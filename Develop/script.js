f0b8ea622e4eaaa1f89a2db3e27ddc7a896a2b97
David De La Rosa <daviddelarosa@Gabrielles-iMac.local>
2023-09-08 21:45:08 -0500

    Created variables for generatePassword function

---
+++ Develop/script.js
@@ -0,0 +1,37 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var characterLength = 8>128;
+var passwordChar = [];
+var lowercaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','w','z'];
+var specialChar = ['!','@','#','$','%','^','&','*','(',')'];
+var upperChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+function generatePassword() {
+ 
+ //need prompt the user for the password criteria
+ //1.Password length 8-128
+ //2. lowercase, uppercase, numbers, special characters
+
+
+
+
+
+ //generate password based on criteria
+ //validate the input
+ //display generated password
+  
+  
+  
+  return "Generated password";
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
